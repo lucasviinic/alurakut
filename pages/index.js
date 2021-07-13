ff35aaeac1fdbe9743d3306771eff88a10547a0f
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-13 18:12:50 -0300

    Conclusão do segundo dia da Imersão React 3a edição
    - Entendendo melhor o que é um SPA
    - Uso do useState
    - Criação do formulário para cadastro de comunidade

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import MainGrid from '../src/components/MainGrid'
-import Box from '../src/components/Box'
-import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import React from 'react';
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
-function ProfileSidebar(props){
-  console.log(props)
-  return(
-    <Box>
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px'}}/>
+function ProfileSidebar(props) {
+  console.log(props);
+  return (
+    <Box as="aside">
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
       <hr />
       <p>
         <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
@@ -17,59 +25,122 @@ function ProfileSidebar(props){
       <hr />
       <AlurakutProfileSidebarMenuDefault />
     </Box>
-  )
+  );
 }
 
 export default function Home() {
-  const githubUser = 'lucasviinic'
+  const githubUser = "lucasviinic";
+  const [comunidades, setComunidades] = React.useState([{
+    id: new Date().toISOString(),
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }])
   const pessoasFavoritas = [
-    'juunegreiros',
-    'omariosouto',
-    'felipefialho',
-    'rafaballerini',
-    'filipealvesdef',
-    'peas'
-  ]
+    "juunegreiros",
+    "omariosouto",
+    "felipefialho",
+    "rafaballerini",
+    "filipealvesdef",
+    "peas",
+  ];
 
   return (
     <>
+      {/* 
+                                  Desafios
+        1. Alterar a imagem de <AlurakutMenu /> para a do usuário atual.
+        2. Colocar imagem padrão, caso o usuário não passe URL ao criar comunidade.
+        3. Não deixar a exibição da lista passar de 6.
+        4. Link para uma comunidade.
+        5. Mudar o plano de fundo para uma imagem de algo que me represente.
+      */}
       <AlurakutMenu />
       <MainGrid>
-        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
           <ProfileSidebar githubUser={githubUser} />
         </div>
-        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
           <Box>
-            <h1 className="title">
-              Bem vindo(a)
-            </h1>
-
+            <h1 className="title">Bem vindo(a)</h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h1 className="subTitle">O que você deseja fazer?</h1>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                const dadosDoForm = new FormData(e.target)
+
+                console.log('Campo: ', )
+                console.log('Campo: ', dadosDoForm.get('image'))
+
+                const comunidade = {
+                  titulo: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('image')
+                }
+
+                setComunidades([...comunidades, comunidade])
+              }}
+            >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  area-aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  area-aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
-        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
           <ProfileRelationsBoxWrapper>
-            <h2 className='smallTitle'>
-              Pessoas favoritas ({pessoasFavoritas.length})
+            <h2 className="smallTitle">
+              Comunidades ({comunidades.length})
+            </h2>
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`} key={itemAtual.title}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
             </h2>
             <ul>
               {pessoasFavoritas.map((pessoa) => {
                 return (
-                  <li>
+                  <li key={pessoa}>
                     <a href={`/users/${pessoa}`} key={pessoa}>
                       <img src={`https://github.com/${pessoa}.png`} />
                       <span>{pessoa}</span>
                     </a>
                   </li>
-                )
+                );
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>
-            Comunidades
-          </Box>
         </div>
       </MainGrid>
     </>
-  )
+  );
 }
